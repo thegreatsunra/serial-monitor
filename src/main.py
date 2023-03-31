dfdd043b96a8552bab1ca316e3f69979dc67777b
Dane Petersen <thegreatsunra@gmail.com>
2023-03-31 08:14:41 -0500

    Format and reorganize script

--- src/main.py
+++ src/main.py
@@ -2,27 +2,33 @@
 """
 Main module docstring
 """
-import serial
 from datetime import datetime
+import serial
+
 
 def hello_world():
-    """Returns 'Hello World'"""
+    """
+    Returns 'Hello World'
+    """
     return "Hello World"
 
 
-def main():
-    """Main entry point of the app"""
+def read_serial_and_write_to_log():
+    """
+    Reads from serial port and writes output to log file
+    """
+    log_file = "/home/ribbon/log.txt"
+    serial_port = "/dev/ttyACM0"
     while True:
         try:
-            logfile = "/home/ribbon/log.txt"
-            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            ser = serial.Serial(port=serial_port, baudrate=9600, timeout=1)
             ser.reset_input_buffer()
             while True:
                 try:
                     if ser.in_waiting > 0:
-                        line = ser.readline().decode('utf-8').rstrip()
+                        line = ser.readline().decode("utf-8").rstrip()
                         print(str(datetime.now()) + " " + line)
-                        with open(logfile, "a") as file:
+                        with open(log_file, mode="a", encoding="utf-8") as file:
                             file.write(str(datetime.now()) + " " + line + "\n")
                             file.close()
                 except (Exception) as exception:
@@ -31,6 +37,13 @@ def main():
         except (Exception) as exception:
             print("Exception: \n", exception)
             continue
+
+
+def main():
+    """
+    Main entry point of the app
+    """
+    read_serial_and_write_to_log()
 
 
 if __name__ == "__main__":
