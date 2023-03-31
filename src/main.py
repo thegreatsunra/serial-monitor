ef854d641236cd1d9164a72c63915d7ad8e7127d
Dane Petersen <thegreatsunra@gmail.com>
2023-03-31 07:56:59 -0500

    Import serial script as-is

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 """
 Main module docstring
 """
-
+import serial
+from datetime import datetime
 
 def hello_world():
     """Returns 'Hello World'"""
@@ -11,7 +12,25 @@ def hello_world():
 
 def main():
     """Main entry point of the app"""
-    print(hello_world())
+    while True:
+        try:
+            logfile = "/home/ribbon/log.txt"
+            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            ser.reset_input_buffer()
+            while True:
+                try:
+                    if ser.in_waiting > 0:
+                        line = ser.readline().decode('utf-8').rstrip()
+                        print(str(datetime.now()) + " " + line)
+                        with open(logfile, "a") as file:
+                            file.write(str(datetime.now()) + " " + line + "\n")
+                            file.close()
+                except (Exception) as exception:
+                    print("Exception: \n", exception)
+                    raise
+        except (Exception) as exception:
+            print("Exception: \n", exception)
+            continue
 
 
 if __name__ == "__main__":
